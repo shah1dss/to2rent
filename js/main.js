853a2b7586d1f4916284bc3eff131a12aac5e737
shah1dss <ckeiter@bk.ru>
2020-07-30 15:49:25 +0300

    Сверстанано 3 блока

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 $(function(){
-    $('')
+    $('.header__slider').slick({
+        infinite: true,
+        fade: true,
+        prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/slider-left.svg" alt="arrows-left">',
+        nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/slider-right.svg" alt="arrows-right">',
+    });
 
 
   
