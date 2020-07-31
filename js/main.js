5b6a95abc3c6df5be8ee9265611c4d1e25b1bcb8
shah1dss <ckeiter@bk.ru>
2020-07-31 22:09:00 +0300

    Сделан адаптив

--- js/main.js
+++ js/main.js
@@ -5,7 +5,4 @@ $(function(){
         prevArrow: '<img class="slider-arrows slider-arrows__left" src="img/slider-left.svg" alt="arrows-left">',
         nextArrow: '<img class="slider-arrows slider-arrows__right" src="img/slider-right.svg" alt="arrows-right">',
     });
-
-
-  
 });
